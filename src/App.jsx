c2e82763d89ce49fe0df7df0093005d4c29a1f85
Anushka <anushkakohli2021@gmail.com>
2024-03-12 23:43:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PDFViewer } from "@react-pdf/renderer";
+import Invoice from "./components/Invoice";
+import PdfCard from "./components/PdfCard";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const cards = {
+    maxWidth: "1200px",
+    margin: "0 auto",
+    display: "grid",
+    gap: "1rem",
+    padding: "20px",
+    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h2 style={{ textAlign: "center" }}>List of invoices</h2>
+      <div style={cards}>
+        <PdfCard title="Oasic ltd Invoice" />
+        <PdfCard title="Libra ltd Invoice" />
+        <PdfCard title="Xpress ltd Invoice" />
+        <PdfCard title="Cardic ltd Invoice" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
